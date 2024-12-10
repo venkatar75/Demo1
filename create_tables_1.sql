9ffb74783a012a0ab2b73410a23bb1ff5f1577fe
venkatar75 <127182386+venkatar75@users.noreply.github.com>
2024-12-10 08:37:04 +0530

    Create create_tables_1.sql
    create dbscripts

---
+++ create_tables_1.sql
@@ -0,0 +1,13 @@
+CREATE TABLE COMPANY_1(
+   ID INT PRIMARY KEY     NOT NULL,
+   NAME           TEXT    NOT NULL,
+   AGE            INT     NOT NULL,
+   ADDRESS        CHAR(50),
+   SALARY         REAL
+);
+
+CREATE TABLE DEPARTMENT_1(
+   ID INT PRIMARY KEY      NOT NULL,
+   DEPT           CHAR(50) NOT NULL,
+   EMP_ID         INT      NOT NULL
+);
